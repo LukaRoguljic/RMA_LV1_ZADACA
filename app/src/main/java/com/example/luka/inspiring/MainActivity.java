90014559fb91bc12be9d372ee512214b1f768239
LukaRoguljic <luka.roguljic16@gmail.com>
2018-03-26 21:13:41 +0200

    Brisanje nepotrebnih importa

--- app/src/main/java/com/example/luka/inspiring/MainActivity.java
+++ app/src/main/java/com/example/luka/inspiring/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.luka.inspiring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
